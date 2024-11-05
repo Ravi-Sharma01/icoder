5fa684f05606da7945bf5bf75554107ab45678a3
Ravi-Sharma01 <ravisharma7357002@gmail.com>
2024-11-05 23:00:45 +0530

    add apps and create many templates for blog website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def blogHome(request):
+    return HttpResponse("this is bloghome. we will keep all the blogposts here")
+
+def blogPost(request, slug):
+    return HttpResponse(f"this is blopost.{slug} ")
