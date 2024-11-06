2ae6685c54e5457e07d719cb4a3b9e357c3dc043
Ravi-Sharma01 <ravisharma7357002@gmail.com>
2024-11-06 17:03:42 +0530

    create blog/templates, and add updates in shop/templates, and add updates in blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def blogHome(request):
-    return HttpResponse("this is bloghome. we will keep all the blogposts here")
+    return render(request, 'blog/blogHome.html')
+#    return HttpResponse("this is bloghome. we will keep all the blogposts here")
 
 def blogPost(request, slug):
     return HttpResponse(f"this is blopost.{slug} ")
